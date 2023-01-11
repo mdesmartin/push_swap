4d0792e20f70a9d89cbdf1f756ea2771fce531e1
Mehdi Vogel <mvogel@z4r8p7.42lyon.fr>
2023-01-11 18:16:25 +0100

    draft

---
+++ parsing.c
@@ -0,0 +1,16 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parsing.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mvogel <mvogel@student.42lyon.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/11 17:32:57 by mvogel            #+#    #+#             */
+/*   Updated: 2023/01/11 17:33:57 by mvogel           ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+recuperer donnes suer une string
+check errors
+separer despace les donnees
+split dans un tableau
